38e36a9f8e80e01a2a96cfda485a01494e5db381
Xifan Tang <u6018939@lnissrv4.eng.utah.edu>
2018-09-13 16:41:36 -0600

    remove useless files

--- trunk/binarynet_tensorflow/android-demo/shaders.h
+++
@@ -1,5 +0,0 @@
-#include <GLES2/gl2.h>
-
-extern char *vertex_shader, *fragment_shader, *fragment_shader_oes;
-
-GLuint compile_program(char *vertex_source, char *fragment_source);
--- trunk/binarynet_tensorflow/compile_benchmark.sh
+++
@@ -1,2 +0,0 @@
-clang-6.0 -target aarch64-linux-gnu -I /usr/aarch64-linux-gnu/include -Wno-builtin-requires-header -mcpu=cortex-a53 ./SRC/benchmark.c benchmark/*.c -ISRC/ -Ibenchmark/ -flto -Ofast -pthread -DPRINT_TIME
-
