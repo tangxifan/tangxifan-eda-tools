eeac276c74aa93309e67ebeb6333be2c5cec898c
tangxifan <tangxifan@gmail.com>
2019-07-04 19:50:25 -0600

    add matlab scripts for tileable_sb_study

---
+++ branches/subvt_fpga/matlab_scripts/tileable_sb_study/with_opposite_wiring/plot_tileable_arch_comp_results.m
@@ -0,0 +1,80 @@
+clear all
+close all
+
+% Define basic string
+arch_type = ["classical", "tileable"];
+sb_type = ["subset", "universal", "wilton"];
+
+% Define path variable
+fdir_name_postfix = ['_fpga'];
+
+% Define file name variable
+fname_postfix = ['_minW1p3_data'];
+
+% Read source data
+idata = 1;
+for iarch = 1:length(arch_type)
+  for isb_type = 1:length(sb_type)
+    if ("classical" == arch_type(iarch)) 
+      sb_subtype = [sb_type(isb_type)];
+    else 
+      sb_subtype = ["subset", "universal", "wilton"];
+    end
+    for isb_subtype = 1:length(sb_subtype)
+      % Create file name 
+      data_fname = arch_type(iarch) + '_' + sb_type(isb_type) ;
+      data_fname = data_fname + '_' + sb_subtype(isb_subtype) ;
+      data_fname = data_fname + fname_postfix; 
+      disp('Reading file ' + data_fname)
+      % change directory 
+      cd(arch_type(iarch) + fdir_name_postfix);
+      % Import the data file to the data base matrix 
+      eval(data_fname);
+      raw_db(idata,:,:) = data; 
+      % back to directory
+      cd('..')
+      % Update name tag 
+      data_name(idata) = arch_type(iarch) + '_' + sb_type(isb_type) + '_' + sb_subtype(isb_subtype);
+      % Pre-process the raw data
+      db(idata,:,:) = cell2mat(raw_db(idata,:,2:end));
+      % increment counter i
+      idata = idata + 1; 
+    end
+  end
+end
+
+%% Pre-process the raw data
+circuit_names = raw_db(1,:,1);
+% Table the area, delay, power
+area = db(:,:,8) + db(:,:,9); % total area = logic_area + routing_area; 
+delay = db(:,:,5);
+minW = db(:,:,2);
+wirelength = db(:,:,10);
+sb_mux_size = db(:,:,14);
+% Average the area, delay and power
+average_area = mean(area');
+average_delay = mean(delay');
+average_minW = mean(minW');
+average_wirelength = mean(wirelength');
+average_sb_mux_size = mean(sb_mux_size');
+% normalize area, delay and power 
+normalize_area = average_area./average_area(1);
+normalize_delay = average_delay./average_delay(1);
+normalize_minW = average_minW./average_minW(1);
+normalize_wirelength = average_wirelength./average_wirelength(1);
+normalize_sb_mux_size = average_sb_mux_size./average_sb_mux_size(1);
+
+%% Bar graphs
+% Normailized Graph
+fig_handle0 = figure;
+b = bar([normalize_area', normalize_delay', normalize_minW']); 
+ch = get(b,'children');
+set(gca,'xlim',[0.5 length(data_name)+0.5],'Fontsize',16);
+set(gca,'ylim',[0.75 1+0.1],'Fontsize',16);
+set(gca,'XTick',1:1:length(data_name));
+set(gca,'XTickLabel', data_name);
+set(fig_handle0, 'Position', [1 1 2000 400]);
+legend([{'Area'};{'Delay'};{'minW'}]);
+ylabel('Normalized Area, Delay and minW');
+grid on
+
